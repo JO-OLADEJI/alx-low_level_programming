5cb0477fd965bf5e87e1055dccae0083cb25d087
Joshua Oladeji <dejijolaoluwa@gmail.com>
2022-06-03 14:56:28 -0700

    printed alphabets in lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description: prints all letters of the alphabet in lowercase
+ *
+ * Return: zero (0) is run successfully
+ */
+int main(void)
+{
+	char alphabets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+	'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+	'y', 'z'};
+	int length = sizeof(alphabets) / sizeof(alphabets[0]);
+	int i;
+
+	for (i = 0; i < length; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
