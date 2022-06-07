a2cb9521dd0384d59827b45a67fd255a76de6043
analogdev.eth <dejijolaoluwa@gmail.com>
2022-06-07 21:37:50 +0100

    conformed to betty coding style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,21 +2,21 @@
 
 /**
  * main - entry function of program
- * 
+ *
  * Description: prints "_putchar" with a new line to the standard output
- * 
+ *
  * Return: 0 (success)
  */
 int main(void)
 {
-  char output[] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r' };
-  int length = sizeof(output) / sizeof(output[0]), i;
+	char output[] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r' };
+	int length = sizeof(output) / sizeof(output[0]), i;
 
-  for (i = 0; i < length; i++)
-  {
-    _putchar(output[i]);
-  }
+	for (i = 0; i < length; i++)
+	{
+		_putchar(output[i]);
+	}
 
-  _putchar('\n');
-  return (0);
+	_putchar('\n');
+	return (0);
 }
