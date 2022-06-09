f18adf453f9250ad01c60c810f7f9e1262de2ab2
analogdev.eth <dejijolaoluwa@gmail.com>
2022-06-09 19:10:15 +0100

    fix betty warnings in 0-isupper.c

--- 0x03-debugging/0-isupper.c
+++ 0x03-debugging/0-isupper.c
@@ -1,18 +1,26 @@
 #include "main.h"
 
+/**
+ * _isupper - function
+ * @c: character to be checked
+ *
+ * Description: checks if a character is uppercase
+ *
+ * Return: 1 if true, 0 otherwise
+ */
 int _isupper(int c)
 {
-  /* uppercase ASCII characters range from 65-90 */
-  int output;
+	/* uppercase ASCII characters range from 65-90 */
+	int output;
 
-  if (c >= 65 && c <=90)
-  {
-    output = 1;
-  }
-  else
-  {
-    output = 0;
-  }
+	if (c >= 65 && c <= 90)
+	{
+		output = 1;
+	}
+	else
+	{
+		output = 0;
+	}
 
-  return (output);
+	return (output);
 }
