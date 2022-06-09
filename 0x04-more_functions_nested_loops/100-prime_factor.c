e11fbea7cc8967c3b743099b94ce04c2f93b4ffa
analogdev.eth <dejijolaoluwa@gmail.com>
2022-06-09 23:45:40 +0100

    more efficient logic for prime factor calculation

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,62 +1,39 @@
 #include <stdio.h>
-
-int is_prime(long);
+#include <math.h>
 
 /**
- * main - entry point of program
- *
- * Description: computes the largest prime factor of the number 612852475143
- *
- * Return: always 0
+ * main - prints the largest prime factor of a number
+ * Return: 0
  */
+
 int main(void)
 {
-	long number = 61285247514L;
-	long half = number / 2;
-	long largest_prime_factor;
-	long i;
+	long int n;
+	long int max;
+	long int i;
 
-	for (i = 1; i <= half; i++)
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
 	{
-		if (number % i == 0 && is_prime(i))
+		max = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
 		{
-			largest_prime_factor = i;
+			max = i;
+			n = n / i;
 		}
 	}
 
-	printf("%ld\n", largest_prime_factor);
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
 	return (0);
-}
-
-/**
- * is_prime - custom function
- * @n: number to be checked
- *
- * Description: checks if a number is prime
- *
- * Return: 1 if prime, 0 otherwise
- */
-int is_prime(long n)
-{
-	int output = 1;
-	long half_n = n / 2;
-	long i;
-
-	if (n == 1)
-	{
-		output = 0;
-	}
-	else
-	{
-		for (i = 2; i <= half_n; i++)
-		{
-			if (n % i == 0)
-			{
-				output = 0;
-				break;
-			}
-		}
-	}
-
-	return (output);
 }
