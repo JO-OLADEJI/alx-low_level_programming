663e458a1baa421cb260bdbf05605474079b2afc
analogdev.eth <dejijolaoluwa@gmail.com>
2022-06-14 05:21:30 +0100

    arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_array(int *a, int n)
+{
+	int i;
+
+  for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n-1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -18,3 +18,5 @@ void rev_string(char *s);
 void puts2(char *str);
 
 void puts_half(char *str);
+
+void print_array(int *a, int n);
