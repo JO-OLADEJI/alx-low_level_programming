0e32f893f4df7c07a21c2e12af1d27f01d3db1a5
analogdev.eth <dejijolaoluwa@gmail.com>
2022-06-14 05:12:18 +0100

    fixed bug in 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * puts_half - function
+ * @str: the string
+ *
+ * Description: prints the last half of the given string
+ *
+ * Return: nothing
+	*/
 void puts_half(char *str)
 {
 	int len;
@@ -15,15 +23,19 @@ void puts_half(char *str)
 	if (len % 2 == 1)
 	{
 		char_print_count = (len - 1) / 2;
+		for (i = char_print_count + 1; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	else
 	{
 		char_print_count = len / 2;
+		for (i = char_print_count; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 
-	for (i = char_print_count; i < len; i++)
-	{
-		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
