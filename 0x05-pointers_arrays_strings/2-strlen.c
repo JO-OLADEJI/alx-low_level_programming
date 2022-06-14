fd2137689bbd90021d00d8b009074cac6a1c3f60
analogdev.eth <dejijolaoluwa@gmail.com>
2022-06-14 04:18:33 +0100

    fix betty warnings in 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strlen - function
+ * @s: pointer to a string
+ *
+ * Description: returns the length of a string
+ *
+ * Return: length of string
+ */
 int _strlen(char *s)
 {
 	int length;
