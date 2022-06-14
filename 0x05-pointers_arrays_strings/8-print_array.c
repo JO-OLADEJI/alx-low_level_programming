200bb475cff8ad2cb63db27b3c0d3d60cf72a8cb
analogdev.eth <dejijolaoluwa@gmail.com>
2022-06-14 05:24:23 +0100

    fixed betty warning for 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
+/**
+ * print_array - function
+ * @a: array of integers
+ * @n: number of elements to print
+ *
+ * Description: prints the first n elements of an array, followed by a new line
+ *
+ * Return: nothing
+	*/
 void print_array(int *a, int n)
 {
 	int i;
 
-  for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n-1)
+		if (i != n - 1)
 		{
 			printf(", ");
 		}
